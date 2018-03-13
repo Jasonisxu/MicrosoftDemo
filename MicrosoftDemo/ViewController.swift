0c306360b9d602b46cda337dd279b47322996196
Wicrenet_Jason <xul2@wicrenet.com>
2018-03-13 21:17:24 +0800

    隐藏电池

--- MicrosoftDemo/ViewController.swift
+++ MicrosoftDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        UIApplication.shared.isStatusBarHidden = true
         self.isNavBarHidden = true
     }
 
